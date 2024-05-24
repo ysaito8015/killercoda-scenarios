b2bc53c8337bcfca34cd2dac8fa705fb4065b264
Yusuke Saito <info@ysaito.dev>
2024-05-24 15:12:18 +0900

    update

--- machine-learning-in-elixir/scenario01/setup.sh
+++ machine-learning-in-elixir/scenario01/setup.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 mkdir -p /root/work
-apt-get update && apt-get install -y tig vim build-essential
-curl https://mise.run | sh
+apt-get update && apt-get install -y tig vim build-essential && \
+curl https://mise.run | sh && \
 echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
