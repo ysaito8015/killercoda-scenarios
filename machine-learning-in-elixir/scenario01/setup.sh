7405c8d713b577228c5ecd1b7e062f98a8c8759b
Yusuke Saito <info@ysaito.dev>
2024-05-24 15:15:30 +0900

    update

--- machine-learning-in-elixir/scenario01/setup.sh
+++ machine-learning-in-elixir/scenario01/setup.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 mkdir -p /root/work
-apt-get update && apt-get install -y tig vim build-essential && \
+apt-get update && apt-get install -y tig vim build-essential autoconf automake libcurses-dev && \
 curl https://mise.run | sh && \
-echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc && \
+source ~/.bashrc
