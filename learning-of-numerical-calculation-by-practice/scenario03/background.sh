1af0e0dc41f52ab04d0b6a197f49cb82868d06fe
Yusuke Saito <info@ysaito.dev>
2024-06-16 21:11:27 +0900

    upte

--- learning-of-numerical-calculation-by-practice/scenario03/background.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-cd "$(dirname "$0")"
-
-rm /var/www/html/index.nginx-debian.html
-
-sed 44p"autoindex on;" /etc/ningx/sites-available/default
--- learning-of-numerical-calculation-by-practice/scenario03/setup.sh
+++ learning-of-numerical-calculation-by-practice/scenario03/setup.sh
@@ -4,3 +4,6 @@ cd "$(dirname "$0")"
 
 mkdir -p /root/work
 apt-get update && apt-get install -y tig vim gfortran gcc nginx
+
+rm /var/www/html/index.nginx-debian.html
+sed 44p"autoindex on;" /etc/ningx/sites-available/default
