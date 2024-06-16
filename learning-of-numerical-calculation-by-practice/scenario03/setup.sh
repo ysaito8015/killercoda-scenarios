784d2d74ad93f5e9037cc6e2f8744f980921472a
Yusuke Saito <info@ysaito.dev>
2024-06-16 21:16:31 +0900

    Update

--- learning-of-numerical-calculation-by-practice/scenario03/setup.sh
+++ learning-of-numerical-calculation-by-practice/scenario03/setup.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 mkdir -p /root/work
-apt-get update && apt-get install -y tig vim gfortran gcc nginx
-
-rm /var/www/html/index.nginx-debian.html
+apt-get update && apt-get install -y tig vim gfortran gcc nginx && \
+rm /var/www/html/index.nginx-debian.html && \
 sed 44p"autoindex on;" /etc/ningx/sites-available/default
